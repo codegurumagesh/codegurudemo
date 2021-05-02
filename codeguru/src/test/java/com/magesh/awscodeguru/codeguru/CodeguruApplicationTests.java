b45c7026fe5a8daf8b95f654d546a51edb5bbd7d
Magesh S <mageshstechie@gmail.com>
2021-05-03 00:41:16 +0400

    Initial Commit for Code Review

---
+++ codeguru/src/test/java/com/magesh/awscodeguru/codeguru/CodeguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magesh.awscodeguru.codeguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
